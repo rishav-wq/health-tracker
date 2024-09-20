8a60ef731a98ec2341c246b1682a14b749364f35
rishav_raj <145801777+rishav-wq@users.noreply.github.com>
2024-09-21 00:16:37 +0530

    Update AddHealthrecord.jsx

--- client/src/components/addHealthRecord/AddHealthrecord.jsx
+++ client/src/components/addHealthRecord/AddHealthrecord.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './addHealthRecord.scss';
+import './addHealthrecord.scss';
 
 const AddHealthRecord = ({ onRecordAdded }) => {
   const [newRecord, setNewRecord] = useState({
